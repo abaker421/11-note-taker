dfe3664b64991190859b37bef9e113278f739308
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-20 16:29:13 -0500

    added delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ const fs = require('fs/promises')
 const { v4: uuidv4 } = require('uuid')
 
 
-app.get('/api/notes', async (req,res) => {
+router.get('/api/notes', async (req,res) => {
     try{
     const notes = await fs.readFile(path.join(__dirname, "..", "db", "db.json"))
     res.send(JSON.parse(notes))
@@ -14,7 +14,7 @@ app.get('/api/notes', async (req,res) => {
     }
 })
 
-app.post('/api/notes', async (req,res) => {
+router.post('/api/notes', async (req,res) => {
     try{
     const content= readFile(path.join(__dirname,'..', 'db', 'db.json', 'utf-8'))
     const notes= JSON.parse(content)
@@ -35,6 +35,27 @@ app.post('/api/notes', async (req,res) => {
     } catch (err) {
         res.status(500).send(err)
         console.log(err)
+    }
+  })
+
+  router.delete('/api/notes/:id', async (req, res) => {
+    const { id } = req.params
+  
+    try {
+    
+      const content = await fs.readFile(path.join(__dirname,'..', 'db', 'db.json', 'utf-8'))
+      const notes = JSON.parse(content)
+  
+      // Find and remove the note with the provided id
+      const updatedNotes = notes.filter(note => note.id !== id)
+  
+      // Write updated data back to db.json
+      await fs.writeFile(path.join(__dirname,'..', 'db', 'db.json'), JSON.stringify(updatedNotes))
+  
+      res.status(200).json({ message: 'Note deleted successfully' })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ error: 'Internal server error' })
     }
   })
 
