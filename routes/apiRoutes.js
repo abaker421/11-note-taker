29a41f96c2db9a82a43960d0569fff6f5639705b
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-20 15:56:07 -0500

    added apiRoutes, need to generate unique ID yet

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,29 @@
 const router= require('express').Router()
+const path = require('path')
+const fs = require('fs')
+
+
+app.get('/api/notes', async (req,res) => {
+    const notes = await fs.readFile('./db/db.json','utf8')
+    res.send(JSON.parse(notes))
+})
+
+app.post('/api/create-pet', async (req,res) => {
+
+    const content= readFile(path.join(__dirname,'..', 'db', 'db.json', 'utf-8'))
+    const notes= JSON.parse(content)
+    
+    
+    const newNote= {
+      ...req.body,
+      id:
+    }
+    
+    notes.push(newNote)
+    
+    await writeFile(path.join(__dirname, '..', 'db', 'db.json', 'utf-8'), JSON.stringify(notes, null, 2))
+    
+    res.stauts(201).json(newNote)
+  })
 
 module.exports = router
