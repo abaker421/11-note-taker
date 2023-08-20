56d99305751829ed6d3211eadb17f800d50364f8
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-20 15:43:30 -0500

    added pageRoutes

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express= require('express')
 const pageRoutes= require('./routes/pageRoutes')
-const apiRpoutes= require('./routes/apiRoutes')
+const apiRoutes= require('./routes/apiRoutes')
 const PORT= 3000
+const db = require('./db/db.json')
+const path = require('path')
+
 
 
 const app= express()
 app.listen(express.static('public'))
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use(pageRoutes)
